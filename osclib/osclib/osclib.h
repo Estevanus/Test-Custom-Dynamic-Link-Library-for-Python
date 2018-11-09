381b003c83ae634769bb7ae70495f8e1aab82119
Estevanus <oscartoreh@gmail.com>
2018-11-09 10:59:31 +0800

    Add files via upload

---
+++ osclib/osclib/osclib.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Python.h"
+
+#ifdef OSCLIB_EXPORTS
+#define OSCLIB_API __declspec(dllexport)
+#else
+#define OSCLIB_API __declspec(dllexport)
+#endif // OSCLIB_EXPORTS
+
+extern "C" OSCLIB_API int kali(int a, int b);
+//int kali(int a, int b);
+//extern "C" OSCLIB_API double kali2(double x, double y);
+extern "C" OSCLIB_API float kali2(float a, float b);
+extern "C" OSCLIB_API double *kali4(double a, double b);
+extern "C" OSCLIB_API double kali5(double a, double b);
+extern "C" OSCLIB_API int loopAll(int max);
+//extern "C" OSCLIB_API PyObject kali3(float a, PyObject b);
+
+
